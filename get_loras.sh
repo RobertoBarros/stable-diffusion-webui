2249595bc31b8f8f55a656206e1d1f6ecbfb3fd6
Roberto Barros <roberto.f.barros@gmail.com>
2023-08-08 23:56:20 -0300

    new models, loras and extensions

---
+++ get_loras.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+DEST_FOLDER="/home/sd/stable-diffusion-webui/models/lora/"
+
+# Old Time Post Apocalyptic Enviroment XL
+# https://civitai.com/models/120651/old-time-post-apocalyptic-enviroment-xl
+wget -nc -O "${DEST_FOLDER}PostApocalypticXL_v1.jpeg" "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/a9fcbd3a-7f1c-4064-a26c-bce432f6702b/width=450/00176-975223051-0000.jpeg"
+wget -nc --content-disposition -P "${DEST_FOLDER}" "https://civitai.com/api/download/models/131229"
+
+
+# epi_noiseoffset
+# https://civitai.com/models/13941/epinoiseoffset
+wget -nc -O "${DEST_FOLDER}epi_noiseoffset2.jpeg" "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/1068e7d8-e7ed-49a9-ae61-ae7e7cbb0f00/width=450/167154.jpeg"
+wget -nc --content-disposition -P "${DEST_FOLDER}" "https://civitai.com/api/download/models/16576"
+
+# The forest light
+# https://civitai.com/models/104292/the-forest-light
+wget -nc -O "${DEST_FOLDER}slg_v30.jpeg" "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/38ba1605-b111-4072-a97f-352fa63f887d/width=450/35349-2897671810-1girl,slg,path,lob,%20longpao,beautiful%20long%20legs,horizon-centered,%20day%20,%F0%9F%A4%AF,.jpeg"
+wget -nc --content-disposition -P "${DEST_FOLDER}" "https://civitai.com/api/download/models/127699"
+
+# Detail Tweaker XL
+# https://civitai.com/models/120651/detail-tweaker-xl
+wget -nc -O "${DEST_FOLDER}add-detail-xl.jpeg" "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/270b159f-d91b-424b-8ac6-14857726f1f1/width=2698/xyz_grid-0004-4138801743.jpeg"
+wget -nc --content-disposition -P "${DEST_FOLDER}" "https://civitai.com/api/download/models/135867"
+
+# Skin & Hands (male/female) from Polyhedron
+# https://civitai.com/models/109043/skin-and-hands-malefemale-from-polyhedron
+wget -nc -O "${DEST_FOLDER}polyhedron_new_skin_v1.1.jpeg" "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/b5787aa0-02d7-4401-abaa-8f81a624910a/width=450/00754-2094697662.jpeg"
+wget -nc --content-disposition -P "${DEST_FOLDER}" "https://civitai.com/api/download/models/135867"
+
+# LowRA
+# https://civitai.com/models/48139/lowra
+wget -nc -O "${DEST_FOLDER}LowRA.jpeg" "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/82e73369-a723-456c-869f-07e9abd48a19/width=450/3.jpeg"
+wget -nc --content-disposition -P "${DEST_FOLDER}" "https://civitai.com/api/download/models/63006"
--- get_models.sh
+++ get_models.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 DEST_FOLDER="/home/sd/stable-diffusion-webui/models/Stable-diffusion/"
 
+wget -nc --content-disposition -P "${DEST_FOLDER}" "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors"
+
 # dreamshaper_8.safetensors
-wget -O "${DEST_FOLDER}dreamshaper_8.jpeg" "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/c1033497-007c-4a73-b812-915c8e32e8fe/width=450/26072224-5775713-(masterpiece),%20(extremely%20intricate_1.3),%20(realistic),%20portrait%20of%20a%20girl,%20the%20most%20beautiful%20in%20the%20world,%20(medieval%20armor),%20me.jpeg"
-wget --content-disposition -P "${DEST_FOLDER}" "https://civitai.com/api/download/models/128713"
+wget -nc -O "${DEST_FOLDER}dreamshaper_8.jpeg" "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/c1033497-007c-4a73-b812-915c8e32e8fe/width=450/26072224-5775713-(masterpiece),%20(extremely%20intricate_1.3),%20(realistic),%20portrait%20of%20a%20girl,%20the%20most%20beautiful%20in%20the%20world,%20(medieval%20armor),%20me.jpeg"
+wget -nc --content-disposition -P "${DEST_FOLDER}" "https://civitai.com/api/download/models/128713"
 
 # absolutereality_v181.safetensors
-wget -O "${DEST_FOLDER}absolutereality_v181.jpeg" "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/734a8ca9-287e-41c4-808e-ea8ce91f4956/width=450/26072067-1584580292-masterpiece,%20(photorealistic_1.4),%20best%20quality,%20beautiful%20lighting,%20(ulzzang-6500_0.5),%20lucy%20_(cyberpunk_),%201girl,%20white%20hair,.jpeg"
-wget --content-disposition -P "${DEST_FOLDER}" "https://civitai.com/api/download/models/132760"
+wget -nc -O "${DEST_FOLDER}absolutereality_v181.jpeg" "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/734a8ca9-287e-41c4-808e-ea8ce91f4956/width=450/26072067-1584580292-masterpiece,%20(photorealistic_1.4),%20best%20quality,%20beautiful%20lighting,%20(ulzzang-6500_0.5),%20lucy%20_(cyberpunk_),%201girl,%20white%20hair,.jpeg"
+wget -nc --content-disposition -P "${DEST_FOLDER}" "https://civitai.com/api/download/models/132760"
 
 # realisticVisionV51_v51VAE.safetensors
-wget -O "${DEST_FOLDER}realisticVisionV51_v51VAE.jpeg" "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/d2828878-dfda-4bda-bcb0-15cc3f6663c1/width=450/00000-3898569358.jpeg"
-wget --content-disposition -P "${DEST_FOLDER}" "https://civitai.com/api/download/models/130072"
+wget -nc -O "${DEST_FOLDER}realisticVisionV51_v51VAE.jpeg" "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/d2828878-dfda-4bda-bcb0-15cc3f6663c1/width=450/00000-3898569358.jpeg"
+wget -nc --content-disposition -P "${DEST_FOLDER}" "https://civitai.com/api/download/models/130072"
 
 # deliberate_v2.safetensors
-wget -O "${DEST_FOLDER}deliberate_v2.jpeg" "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/40943b9c-ede2-48c0-77d7-b48fee661d00/width=450/150224.jpeg"
-wget --content-disposition -P "${DEST_FOLDER}" "https://civitai.com/api/download/models/15236"
+wget -nc -O "${DEST_FOLDER}deliberate_v2.jpeg" "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/40943b9c-ede2-48c0-77d7-b48fee661d00/width=450/150224.jpeg"
+wget -nc --content-disposition -P "${DEST_FOLDER}" "https://civitai.com/api/download/models/15236"
+
+# photon_v1.safetensors
+wget -nc -O "${DEST_FOLDER}photon_v1.jpeg" "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/4a3bf887-9e0e-482f-bb33-e42adb34ba00/width=450/00155-379732800.jpeg"
+wget -nc --content-disposition -P "${DEST_FOLDER}" "https://civitai.com/api/download/models/90072"
